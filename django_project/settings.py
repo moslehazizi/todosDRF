9335fd3953b3f9f0a28c442e39564476919b15b8
mosleh <moslehazizi@gmail.com>
2024-02-05 22:47:45 +0330

    set model and test it

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party
+    'rest_framework',
     # Local
     'todos.apps.TodosConfig'
 ]
@@ -123,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Todo
 
-# Create your tests here.
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title = "First test todo",
+            body = "Todo body for test", 
+        )
+    
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, "First test todo")
+        self.assertEqual(self.todo.body, "Todo body for test")
+        self.assertEqual(str(self.todo), "First test todo")
